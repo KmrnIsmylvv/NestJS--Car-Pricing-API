00052ec460b244095e22f40e1cc7f5eda0c6d331
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-05-10 12:01:32 +0400

    feat: added Report entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,14 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [User],
+      entities: [User, Report],
       synchronize: true,
     }),
     UsersModule,
