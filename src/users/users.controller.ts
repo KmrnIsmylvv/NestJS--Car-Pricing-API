875dcb0a234965f919bca891b74561d2a40f5c62
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-05-22 16:10:20 +0400

    feat: created custom Decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,6 +16,7 @@ import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -25,9 +26,14 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+  // @Get('/whoami')
+  // whoAmI(@Session() session: any) {
+  //   return this.usersService.findOne(session.userId);
+  // }
+
   @Get('/whoami')
-  whoAmI(@Session() session: any) {
-    return this.usersService.findOne(session.userId);
+  whoaAmI(@CurrentUser() user: string) {
+    return user;
   }
 
   @Post('/signout')
