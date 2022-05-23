6d7b081730ae9cc6795c8100be2c25a7a86294db
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-05-23 12:05:24 +0400

    feat: Unit Testing Setup

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,24 @@
+import { Test } from '@nestjs/testing';
+import { async } from 'rxjs';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+
+it('can create an instance of auth service', async () => {
+  // Create a fake copy of the users service
+  const fakeUsersService = {
+    find: () => Promise.resolve([]),
+    create: (email: string, password: string) =>
+      Promise.resolve({ id: 1, email, password }),
+  };
+
+  const module = await Test.createTestingModule({
+    providers: [
+      AuthService,
+      { provide: UsersService, useValue: fakeUsersService },
+    ],
+  }).compile();
+
+  const service = module.get(AuthService);
+
+  expect(service).toBeDefined();
+});
