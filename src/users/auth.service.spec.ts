7ade795ec768b41d4ed92faf60f9f62265de0692
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-05-24 11:31:55 +0400

    refactor: Update auth service testing

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,24 +1,32 @@
-import { Test } from '@nestjs/testing';
-import { async } from 'rxjs';
-import { AuthService } from './auth.service';
-import { UsersService } from './users.service';
+import {Test} from '@nestjs/testing';
+import {async} from 'rxjs';
+import {AuthService} from './auth.service';
+import {UsersService} from './users.service';
+import {User} from "./user.entity";
 
-it('can create an instance of auth service', async () => {
-  // Create a fake copy of the users service
-  const fakeUsersService = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
-  };
+describe('AuthService', () => {
+    let service: AuthService;
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      { provide: UsersService, useValue: fakeUsersService },
-    ],
-  }).compile();
+    beforeEach(async () => {
+        // Create a fake copy of the users service
+        const fakeUsersService: Partial<UsersService> = {
+            find: () => Promise.resolve([]),
+            create: (email: string, password: string) =>
+                Promise.resolve({id: 1, email, password} as User),
+        };
 
-  const service = module.get(AuthService);
+        const module = await Test.createTestingModule({
+            providers: [
+                AuthService,
+                {provide: UsersService, useValue: fakeUsersService},
+            ],
+        }).compile();
 
-  expect(service).toBeDefined();
+        const service = module.get(AuthService);
+    });
+
+    it('can create an instance of auth service', async () => {
+        expect(service).toBeDefined();
+    });
 });
+
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -7,7 +7,7 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-    }).compile();
+      }).compile();
 
     controller = module.get<UsersController>(UsersController);
   });
