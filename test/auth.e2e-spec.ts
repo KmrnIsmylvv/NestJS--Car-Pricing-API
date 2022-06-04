5abdc4b1256db799e816e92dc2eed108fffceebb
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-06-04 21:41:32 +0400

    feat: Creating End to End Testing

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,32 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import exp from 'constants';
+
+describe('Authentication System', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('handles a signup request', () => {
+    const email = 'askd@asm.com';
+
+    return request(app.getHttpServer())
+      .post('auth/signup')
+      .send({ email, password: 'asksdk' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
+});
