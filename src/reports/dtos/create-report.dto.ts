43e9d98dff250c832635c2adaf40fab681defd35
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-06-13 20:36:29 +0400

    feat: Adding Reporst Controller and CreateReportDto

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,15 @@
+export class CreateReportDto {
+  make: string;
+
+  model: string;
+
+  year: number;
+
+  mileage: number;
+
+  lng: number;
+
+  lat: number;
+
+  price: number;
+}
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  @Post()
+  createReport(@Body() body: CreateReportDto) {}
+}
