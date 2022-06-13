4e7c5bcd2c53c807e45c4d31510892cdf4f84702
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-06-13 21:58:10 +0400

    feat: Creating Report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,8 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Controller('reports')
 export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
   @Post()
-  createReport(@Body() body: CreateReportDto) {}
+  @UseGuards(AuthGuard)
+  createReport(@Body() body: CreateReportDto) {
+    return this.reportsService.create(body);
+  }
 }
